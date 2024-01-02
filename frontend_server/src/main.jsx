28be3b158a534b30b0b1fd6d57009277bf521acb
Peter Kimutai <135107576+audirebocha@users.noreply.github.com>
2024-01-02 08:52:03 +0300

    Update main.jsx

--- frontend_server/src/main.jsx
+++ frontend_server/src/main.jsx
@@ -9,14 +9,8 @@ import {
 import Index from './pages/index.jsx';
 
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element: <Index />,
-  },
-  
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>,
 )
